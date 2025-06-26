a65698994a0eb3194224741863f853d1532f8cf6
sahiltaksal <sahiltaksal07@gmail.com>
2025-06-26 22:10:11 +0530

    26/6/2025

---
+++ inverted_right.java
@@ -0,0 +1,13 @@
+
+
+public class inverted_right {
+    public static void main(String[] args) {
+        int num=5;
+        for(int i=1;i<=num;i++){
+            for(int j=5;j>=i;j--){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
