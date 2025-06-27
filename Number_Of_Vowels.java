efa3e3cbf9ee4da72ca8a6ce2e3da57409f72b11
sahiltaksal <sahiltaksal07@gmail.com>
2025-06-27 23:55:50 +0530

    27/6/25

---
+++ Number_Of_Vowels.java
@@ -0,0 +1,17 @@
+public class Number_Of_Vowels {
+
+    public static void main(String[] args) {
+        char a ='D';
+        char b= 'R';
+        int count=0;
+        for(char d='D'; d<= 'R';d++){
+            if (d=='A' || d=='E' || d=='I' || d=='O' || d=='U') {
+                count++;
+           
+            }
+            
+
+        }
+        System.out.println(count);
+    }
+}
--- armstrongnumber.java
+++ armstrongnumber.java
@@ -1,4 +1,4 @@
-
+package java;
 public class armstrongnumber {
     
     public static void main(String[] args) {
