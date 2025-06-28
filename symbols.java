243a1574c59c2a71da2fbf5437f0f55a065a945d
sahiltaksal <sahiltaksal07@gmail.com>
2025-06-28 23:02:03 +0530

    28/6/2025

---
+++ symbols.java
@@ -0,0 +1,14 @@
+package java;
+
+//program to print all symbols whose ascii value is in between 1 to 25
+public class symbols {
+
+   public static void main(String[] args) {
+    for(int i=1;i<=25;i++){
+         System.out.println((char)(i));
+
+
+    }
+   }
+
+    }
