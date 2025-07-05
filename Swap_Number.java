6a17fff6c86a0da392f2b1c77e143f356df564a2
sahiltaksal <sahiltaksal07@gmail.com>
2025-07-05 17:04:27 +0530

    5/7/2025

--- Swap_Number.java
+++ Swap_Number.java
@@ -3,12 +3,14 @@ public class Swap_Number {
     public static void main(String[] args) {
         int a=10 ;
         int b=20 ;
-      
+      System.out.println("Before swapping the value A: "+a);
+      System.out.println("Before swapping the value B: "+b);
+
          a= a+b;
          b=a-b;
          a=a-b;
-         System.out.println(a);
-         System.out.println(b);
+         System.out.println("After swapping the value A: "+a);
+         System.out.println("After swapping the value B: "+b);
 
     }
 }
