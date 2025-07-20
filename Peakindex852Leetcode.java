bcf4ee0de967bb3f1f8720123facb9125d7baa62
sahiltaksal <sahiltaksal07@gmail.com>
2025-07-20 18:35:33 +0530

    leet code problem peak index

---
+++ Peakindex852Leetcode.java
@@ -0,0 +1,28 @@
+
+//https://leetcode.com/problems/peak-index-in-a-mountain-array/?envType=problem-list-v2&envId=array
+public class Peakindex852Leetcode {
+
+     public static void main(String[] args) {
+        int arr[]={0,1,0};
+        System.out.println(peakIndexInMountainArray(arr));
+
+     }
+     public static int peakIndexInMountainArray(int[] arr) {
+         int start=0;
+       int end=arr.length-1;
+        
+        while (start<end) {
+            int mid = start+(end-start)/2;
+
+            if (arr[mid]>arr[mid+1]) {
+                end= mid;
+                
+            }else{
+
+                start=mid+1;
+            }
+            
+        }
+        return start;
+    }
+}
