b50cf54b8d33fc15807e74e48280e8c7bf222818
sahiltaksal <sahiltaksal07@gmail.com>
2025-07-22 23:12:28 +0530

    not solved yet

--- leet.java
+++ leet.java
@@ -1,3 +1,11 @@
-public class leet code  {
-    
+//https://leetcode.com/problems/rotate-array/description/
+public class leet   {
+    public static void main(String[] args) {
+        System.out.println("not getting answer ");
+
+for(int i=0 ;i<10;i++){
+System.out.println(i);
+}
+
+    }
 }
